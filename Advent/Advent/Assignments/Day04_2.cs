873581eb694b98e3116d60eb2a3215d59d715d98
Acc3ss Violation <wrdbrookhuis@gmail.com>
2023-12-05 18:20:15 +0100

    Day 4 Part 2

--- Advent/Advent/Assignments/Day04_2.cs
+++ Advent/Advent/Assignments/Day04_2.cs
@@ -1,22 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Advent.Assignments
 {
-    internal class Day04_1 : IAssignment
+    internal class Day04_2 : IAssignment
     {
         public string Run(IReadOnlyList<string> input)
         {
             int WinStart = input.Count > 10 ? 10 : 8;
-            int WinCount = input.Count > 10 ? 10 : 5;
-            int GameStart = input.Count > 10 ? 42 : 25;
-            int GameCount = input.Count > 10 ? 25 : 8;
+            int WinCount = input.Count > 10 ? 10: 5;
+            int GameStart = input.Count > 10 ? 42:  25;
+            int GameCount = input.Count > 10 ? 25: 8;
 
             var wins = new int[WinCount];
-            var totalScore = 0;
+            var bonusCards = new int[input.Count];
+            var cardIndex = 0;
+            var totalCards = 0;
 
             foreach (var cardLine in input)
             {
@@ -25,18 +28,27 @@ namespace Advent.Assignments
                     wins[i] = ParseDoubleDigitInt(cardLine.AsSpan(WinStart + i * 3));
                 }
 
-                var score = 1;
+                var score = 0;
                 for (var i = 0; i < GameCount; i++)
                 {
                     var num = ParseDoubleDigitInt(cardLine.AsSpan(GameStart + i * 3));
                     if (wins.Contains(num))
-                        score <<= 1;
+                        score++;
                 }
 
-                score >>= 1;
-                totalScore += score;
+                //Logger.DebugLine($"Card {cardIndex + 1} wins {score} cards and is repeated {bonusCards[cardIndex]} times");
+                var totalTimes = bonusCards[cardIndex] + 1;
+
+                for (var i = cardIndex + 1; i < cardIndex + 1 + score && i < bonusCards.Length; i++)
+                {
+                    //Logger.DebugLine($"\tBonus card {i + 1} added {totalTimes} times");
+                    bonusCards[i] += totalTimes;
+                }
+
+                totalCards += totalTimes;
+                cardIndex++;
             }
-            return totalScore.ToString();
+            return totalCards.ToString();
         }
 
         public static int ParseDoubleDigitInt(ReadOnlySpan<char> str)
