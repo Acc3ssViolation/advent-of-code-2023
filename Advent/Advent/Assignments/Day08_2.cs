28d69e52b493adfd9fc675b606932d71d18a10bc
Acc3ss Violation <wrdbrookhuis@gmail.com>
2023-12-08 21:35:11 +0100

    Day 8 Part 2

---
+++ Advent/Advent/Assignments/Day08_2.cs
@@ -0,0 +1,55 @@
+﻿using Advent.Shared;
+
+namespace Advent.Assignments
+{
+    internal class Day08_2 : IAssignment
+    {
+        string IAssignment.TestFile => "test-day08_2.txt";
+
+        private record Node(string Name, string Left, string Right);
+
+        public string Run(IReadOnlyList<string> input)
+        {
+            var dict = new Dictionary<string, Node>();
+
+            var commands = input[0];
+
+            var startPoints = new List<string>();
+
+            foreach (var line in input.Skip(2))
+            {
+                var key = line.Substring(0, 3);
+                var left = line.Substring(7, 3);
+                var right = line.Substring(12, 3);
+
+                if (key[2] == 'A')
+                    startPoints.Add(key);
+
+                dict.Add(key, new Node(key, left, right));
+            }
+
+            var stepsPerStart = new long[startPoints.Count];
+
+            for (var i = 0; i < startPoints.Count; i++)
+            {
+                var steps = 0;
+                var place = startPoints[i];
+                var commandIndex = 0;
+                while (place[2] != 'Z')
+                {
+                    var options = dict[place];
+                    var command = commands[commandIndex++];
+                    if (commandIndex >= commands.Length)
+                        commandIndex = 0;
+                    place = command == 'L' ? options.Left : options.Right;
+                    steps++;
+                }
+                stepsPerStart[i] = steps;
+            }
+            var totalSteps = stepsPerStart.LowestCommonMultiple();
+
+            return totalSteps.ToString();
+        }
+
+    }
+}
---
+++ Advent/Advent/Shared/MathExtra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent.Shared
+{
+    internal static class MathExtra
+    {
+        public static long LowestCommonMultiple(this IEnumerable<long> nums)
+        {
+            return nums.Aggregate(LowestCommonMultiple);
+        }
+
+        public static long LowestCommonMultiple(long a, long b)
+        {
+            return Math.Abs(a * b) / GreatestCommonDivisor(a, b);
+        }
+
+        public static long GreatestCommonDivisor(long a, long b)
+        {
+            if (b == 0)
+                return a;
+            return GreatestCommonDivisor(b, a % b);
+        }
+    }
+}
